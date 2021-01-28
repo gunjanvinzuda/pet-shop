b81b75a1cad4a9b2d34f8eb97dca455093e394e0
Gunjan Vinzuda <58952163+gunjanvinzuda@users.noreply.github.com>
2021-01-28 22:04:28 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -44,12 +44,13 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
+
     return App.initContract();
   },
 
   initContract: function() {
     $.getJSON('Adoption.json', function(data) {
-    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    // Get the necessary contract artifact file and instantiate it with @truffle/contract
     var AdoptionArtifact = data;
     App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
@@ -67,7 +68,7 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
+  markAdopted: function() {
     var adoptionInstance;
 
     App.contracts.Adoption.deployed().then(function(instance) {
@@ -109,7 +110,7 @@ App = {
       }).catch(function(err) {
         console.log(err.message);
       });
-    });
+    });  
   }
 
 };
